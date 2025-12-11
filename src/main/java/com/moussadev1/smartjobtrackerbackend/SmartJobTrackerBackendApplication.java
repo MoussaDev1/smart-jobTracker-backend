8052bb4c61cd59d097f4d79c3cb6ff02d2ff8c16
Moussa <mx_93@device-275.home>
2025-12-11 15:05:12 +0100

    Initial commit

---
+++ src/main/java/com/moussadev1/smartjobtrackerbackend/SmartJobTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moussadev1.smartjobtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartJobTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartJobTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moussadev1/smartjobtrackerbackend/SmartJobTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moussadev1.smartjobtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartJobTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
